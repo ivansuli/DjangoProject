0a2a6ef3a9efbc414079a769f2f47cfdda7cf270
ivansuli <galarzaivan016@gmail.com>
2024-06-24 21:41:00 -0300

    first pages

--- Real_State/Real_State/settings.py
+++ Real_State/Real_State/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Real_State.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
